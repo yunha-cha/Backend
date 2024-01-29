68692fd13773281392bf205c6306a198db9f142c
JunKyeong-Lee <worldreaming@gmail.com>
2024-01-29 13:06:04 +0900

    캘린더 패키지 임시완료 #6, 캘린더 sql event 완료 #7

--- src/main/java/com/wittypuppy/backend/calendar/repository/EmployeeRepository.java
+++ src/main/java/com/wittypuppy/backend/calendar/repository/EmployeeRepository.java
@@ -4,10 +4,11 @@ import com.wittypuppy.backend.calendar.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Repository("Calendar_EmployeeRepository")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-//    List<Employee> findAllByEmployeeRetire
+    List<Employee> findAllByEmployeeRetirementDateBefore(LocalDateTime currentDateTime);
 }
