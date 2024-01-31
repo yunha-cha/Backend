196627f56341b3ae41f46fd1a288b8078ea7ac87
kim-sungmin <kimmin699@gmail.com>
2024-01-31 17:20:33 +0900

    로그인 회원가입 restapi

---
+++ src/main/java/com/wittypuppy/backend/auth/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package com.wittypuppy.backend.auth.filter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wittypuppy.backend.Employee.dto.EmployeeDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    /*
+    * 필터는 일반적인 HTTP 요청 및 응답의 가로채기와 수정에 사용되고,
+    * 인터셉터는 주로 컨트롤러 메서드의 호출 전후 동작을 추가하기 위해 사용됩니다.
+    * */
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super.setAuthenticationManager(authenticationManager);
+    }
+    /**
+     * 지정된 url 요청시 해당 요청을 가로채서 검증 로직을 수행하는 메서드
+     * */
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        UsernamePasswordAuthenticationToken authRequest;
+
+        try {
+            authRequest = getAuthRequest(request);
+            setDetails(request, authRequest);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return this.getAuthenticationManager().authenticate(authRequest);
+    }
+    /**
+     * 사용자의 로그인 리소스 요청시 요청 정보를 임시 토큰에 저장하는 메서드
+     *
+     * @param request - httpServletRequest
+     * @return UserPasswordAuthenticationToken
+     * @throw Excpetion e
+     * */
+    private UsernamePasswordAuthenticationToken getAuthRequest(HttpServletRequest request) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        objectMapper.configure(JsonParser.Feature.AUTO_CLOSE_SOURCE,true);
+        EmployeeDTO employeeDTO = objectMapper.readValue(request.getInputStream(), EmployeeDTO.class);
+
+        return new UsernamePasswordAuthenticationToken(employeeDTO.getEmployeeId(), employeeDTO.getPassword());
+    }
+
+}
---
+++ src/main/java/com/wittypuppy/backend/auth/handler/CustomAuthSuccessHandler.java
@@ -0,0 +1,59 @@
+package com.wittypuppy.backend.auth.handler;
+
+import com.wittypuppy.backend.common.AuthConstants;
+import com.wittypuppy.backend.Employee.dto.EmployeeDTO;
+import com.wittypuppy.backend.Employee.dto.TokenDTO;
+import com.wittypuppy.backend.util.ConvertUtil;
+import com.wittypuppy.backend.util.TokenUtils;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.json.simple.JSONObject;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+@Configuration
+public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+        EmployeeDTO employee  = ((EmployeeDTO) authentication.getPrincipal());
+
+        HashMap<String, Object> responseMap = new HashMap<>();
+        JSONObject jsonValue = null;
+        JSONObject jsonObject;
+        if(employee.getEmployeeRetirementDate() != null){   //null이면 일반 사원이고 Null이 아니면 탈퇴계정이다. 바꿔줌
+            responseMap.put("userInfo", jsonValue);
+            responseMap.put("status", 500);
+            responseMap.put("message","탈퇴 상태인 계정입니다.");
+        }else{
+
+            String token = TokenUtils.generateJwtToken(employee);
+            // tokenDTO response
+            TokenDTO tokenDTO = TokenDTO.builder()
+                                .employeeName(employee.getEmployeeName())
+                                .accessToken(token)
+                                .grantType(AuthConstants.TOKEN_TYPE)
+                                .build();
+
+            jsonValue = (JSONObject) ConvertUtil.convertObjectToJsonObject(tokenDTO);
+
+            responseMap.put("userInfo", jsonValue);
+            responseMap.put("status", 200);
+            responseMap.put("message", "로그인 성공");
+        }
+
+        jsonObject = new JSONObject(responseMap);
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonObject);
+        printWriter.flush();
+        printWriter.close();
+    }
+}
