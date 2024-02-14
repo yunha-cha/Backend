aa25df2e585f30882155f8a77b05eb5b8c13d8ce
Seopia <wjdwltjq7289@gmail.com>
2024-02-14 10:10:13 +0900

    풀용 커밋

--- src/main/java/com/wittypuppy/backend/auth/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/wittypuppy/backend/auth/filter/JwtAuthorizationFilter.java
@@ -101,7 +101,7 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
      * 토큰 관련된 Exception 발생 시 예외 응답
      * */
     private JSONObject jsonresponseWrapper(Exception e) {
-        String resultMsg = "";
+        String resultMsg;
         if (e instanceof ExpiredJwtException) {
             resultMsg = "Token Expired";
         } else if (e instanceof SignatureException) {
@@ -113,6 +113,7 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         }
         // 이외 JTW 토큰내에서 오류 발생
         else {
+            e.printStackTrace();
             resultMsg = "OTHER TOKEN ERROR";
         }
 
