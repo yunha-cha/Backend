b1bc476c4dd6d897dcdbdaa94b6af0e9151b1593
SBH05 <seobohye5@gmail.com>
2024-02-22 09:28:15 +0900

    연차 목록 쿼리문 수정 완료

--- src/main/java/com/wittypuppy/backend/attendance/repository/AttendanceAdminEmployee.java
+++ src/main/java/com/wittypuppy/backend/attendance/repository/AttendanceAdminEmployee.java
@@ -29,18 +29,24 @@ public interface AttendanceAdminEmployee extends JpaRepository<AdminEmployee, In
 
 
     @Query(value = "SELECT DISTINCT " +
-            "B.employee_code, " +
             "B.employee_name, " +
+            "B.employee_code, " +
             "B.employee_join_date, " +
             "B.department_code, " +
-            "A.vacation_creation_reason, " +
             "B.employee_assigned_code, " +
+            "A.vacation_creation_reason, " +
             "A.vacation_creation_date, " +
             "C.department_name, " +
             "A.vacation_expiration_date " +
             "FROM tbl_employee B " +
             "LEFT JOIN tbl_vacation A ON A.employee_code = B.employee_code " +
             "LEFT JOIN tbl_department C ON B.department_code = C.department_code " +
-            "WHERE A.vacation_expiration_date IS NULL OR YEAR(A.vacation_expiration_date) = YEAR(NOW())", nativeQuery = true)
+            "WHERE (A.vacation_expiration_date IS NULL " +
+            "       AND DATE_ADD(B.employee_join_date, INTERVAL 1 MONTH) <= CURRENT_DATE()) " +
+            "   OR (DATE_ADD(A.vacation_creation_date, INTERVAL 1 MONTH) <= CURRENT_DATE() " +
+            "       AND DATEDIFF(CURRENT_DATE(), B.employee_join_date) < 365) " +
+            "   OR (DATEDIFF(CURRENT_DATE(), B.employee_join_date) > 365 " +
+            "       AND YEAR(A.vacation_creation_date) < YEAR(CURRENT_DATE()) " +
+            "       AND YEAR(A.vacation_expiration_date) >= YEAR(CURRENT_DATE()))", nativeQuery = true)
     Page<AdminEmployee> findNo(Pageable paging);
 }
